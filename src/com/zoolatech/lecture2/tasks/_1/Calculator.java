a16e9813d83af669b1628493c1f59957c360e048
Dana <danetqt@gmail.com>
2022-11-03 00:23:25 +0200

    Add simple calculator implementation

---
+++ src/com/zoolatech/lecture2/tasks/_1/Calculator.java
@@ -0,0 +1,35 @@
+package com.zoolatech.lecture2.tasks._1;
+
+public class Calculator {
+    private double currentValue;
+    public double getCurrentValue() {
+        return currentValue;
+    }
+    public void setCurrentValue(double currentValue) {
+        this.currentValue = currentValue;
+    }
+
+    public void addition(double nextNumber) {
+        this.currentValue = currentValue + nextNumber;
+        System.out.println(" = " + currentValue);
+    }
+
+    public void subtraction(double nextNumber) {
+        this.currentValue = currentValue - nextNumber;
+        System.out.println(" = " + currentValue);
+    }
+
+    public void multiplication(double nextNumber) {
+        this.currentValue = currentValue * nextNumber;
+        System.out.println(" = " + currentValue);
+    }
+
+    public void division(double nextNumber) {
+        if (nextNumber == 0) {
+            System.out.println("Are you mad? Cannot divide by 0");
+        } else {
+            this.currentValue = currentValue / nextNumber;
+            System.out.println(" = " + currentValue);
+        }
+    }
+}
